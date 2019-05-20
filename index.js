4f319b4d858c337f531cf53977be1cd38691c11f
Mercedes Gaillard <mdg816@gmail.com>
2019-05-20 11:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // write your code below!
+
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+
+function happyHolidayTo(holiday , fname) {
+  console.log(`Happy ${holiday} , ${fname}!`)
+}
+
+
+function holidayCountdown(holidays, days) {
+  console.log(`Its ${days} days until ${holidays}!`)
+}
+
+
+happyHolidays()
+happyHolidaysTo('Mercedes')
+happyHolidayTo('birthday','Nia')
+holidayCountdown('Valentines day','75');
